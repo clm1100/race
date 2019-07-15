63cb5e937f2c7776bf12dc176d0d3be32b2b859f
陈黎明 <275298711@qq.com>
2019-07-15 10:54:37 +0800

    增加路由

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+
+let usersRouter = require("./routes/users");
+
 app.use(bodyParser.urlencoded({extend:false}));
+
+
+app.use(usersRouter)
 
 app.listen(3000,()=>{
     console.log("running http://127.0.0.1:3000");
