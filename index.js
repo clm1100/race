93659efd4dceffc7a8fc034a8576ccb600d026eb
陈黎明 <275298711@qq.com>
2019-07-17 11:16:50 +0800

    重构目录结构

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const bodyParser = require("body-parser");
-
-
+require('dotenv').config();
 let usersRouter = require("./routes/users");
+const template = require("express-art-template");
+
+app.set("views",path.join(__dirname,"views"));
+app.engine('html',template)
+app.set("view engine","html");
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.use(bodyParser.urlencoded({extend:false}));
 
-
-app.use(usersRouter)
+// app.use(usersRouter)
 
 app.listen(3000,()=>{
     console.log("running http://127.0.0.1:3000");
